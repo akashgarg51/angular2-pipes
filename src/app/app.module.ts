7843d452455904fe09d140c67de0217ed17049e6
Akash Garg <akash.garg@sap.com>
2017-06-18 19:26:08 +0530

    Custom Pipes
    Change-Id: I392b8ef994052406edf706bb1e2e5367dabca378
    Signed-off-by: Akash Garg <akash.garg@sap.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { PipesAppComponent } from './pipes.component';
+import { DoublePipe } from './double.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PipesAppComponent  
+    PipesAppComponent,
+    DoublePipe  
   ],
   imports: [
     BrowserModule,
--- src/app/pipes.component.ts
+++ src/app/pipes.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pipes-app',
-  templateUrl: 'pipes.component.html',
-  styles: [`
-  `]
+    selector: 'pipes-app',
+    templateUrl: 'pipes.component.html',
+    styles: [`
+     .pipes {
+    margin: 32px;
+     padding: 32px;
+}
+  `],
+
 })
 export class PipesAppComponent {
-  myValue = 'lowercase';
-  myDate = new Date(2016,5,24);
+    myValue = 'lowercase';
+    myDate = new Date(2016, 5, 24);
 }
